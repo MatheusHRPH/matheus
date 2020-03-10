2a66fdc739e22acb6c71b78db36b00e7c8a4740a
MatheusHRPH <61998638+MatheusHRPH@users.noreply.github.com>
2020-03-09 22:36:39 -0300

    Add files via upload

---
+++ impar.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+ int num;
+ printf("Informe um inteiro positivo:");
+ scanf("%d", &num);
+ if (num%2==0)
+        printf("%d par\n", num);
+ else
+    printf("%d impar\n", num);
+ system("pause");
+ return 0;
+
+
+
+
+
+
+
+
+}
